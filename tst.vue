aa544a92e84ae00bf7c874b994624588b4826ff7
senlucky <22senlucky@gmail.com>
2018-11-15 13:36:35 +0530

    Create tst.vue

---
+++ tst.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="test">
+    <div>
+      <script>alert(0)</script>
+      <img onerror="alert(1)" src="http://foo.bar/biz" />
+      <h2 onmouseover="alert(2)">Test</h2>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
